398f89764f46bd04f4c1d924297add6e14b33272
Ubuntu <starko07s@withUs.x24y3upmomuujoydb5r4qmk55f.ax.internal.cloudapp.net>
2022-12-20 14:48:57 +0000

    Cors policy fix

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const dotenv = require("dotenv");
 const landingRouter = require('./Routers/mainPageRouter')
 const mongoose = require('mongoose')
 const path = require('path')
-
+const cors = require('cors')
 dotenv.config({path:'./config.env'}) // Config of ENV
+const app = express()
+app.use(cors({
+origin:'*'
+}))
 
 const port = process.env.PORT || 8000 // Port declaration
 const DB = process.env.DATABASE.replace("<password>",process.env.DATABASE_PASSWORD)
 mongoose.connect(DB).then(()=>{console.log("DB connected")})
-const app = express(); // Server start
 
 app.use(morgan("short")); //Logs requests
 app.use(express.json()); //Enables req reading
